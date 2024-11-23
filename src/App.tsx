b949c02fe7c730bc8ed799f1f0533fa916769a4e
muthana-abo-alez1 <92632582+muthana-abo-alez1@users.noreply.github.com>
2024-11-23 18:57:07 +0200

    Revert "Feature dark mode"

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,26 @@
-import React from "react";
-import ThemeSwitcher from "components/ThemeSwitcher";
-import { Button } from "@mui/material";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-const App: React.FC = () => {
+function App() {
   return (
-    <div>
-      <h1>hello</h1>
-      <ThemeSwitcher />
-      <Button>test</Button>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
-};
+}
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import { ThemeProviderWrapper } from "context/ThemeContext";
-import { CssBaseline } from "@mui/material";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+  document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <ThemeProviderWrapper>
-    <CssBaseline />
-      <App />
-    </ThemeProviderWrapper>
+    <App />
   </React.StrictMode>
 );
 
