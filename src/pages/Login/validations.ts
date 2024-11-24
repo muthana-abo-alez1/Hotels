8da2d4046a5c2da964cb20e7ef50727052dc33d5
muthana-abo-alez1 <92632582+muthana-abo-alez1@users.noreply.github.com>
2024-11-24 15:10:56 +0200

    add validation for inputs login page

---
+++ src/pages/Login/validations.ts
@@ -0,0 +1,7 @@
+import * as Yup from 'yup';
+export const loginValidationSchema = Yup.object({
+  username: Yup.string().required('Username is required'),
+  password: Yup.string()
+    .required('Password is required')
+    .min(4, 'Password must be at least 4 characters'),
+});
