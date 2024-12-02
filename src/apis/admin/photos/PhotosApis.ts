2ee6a88c82942d2b9ac4fa4a243311ffe33607a7
muthana-abo-alez1 <92632582+muthana-abo-alez1@users.noreply.github.com>
2024-12-02 15:00:52 +0200

    add implementation photos apis

---
+++ src/apis/admin/photos/PhotosApis.ts
@@ -0,0 +1,34 @@
+import { axiosInstance, handleError } from "apis/ApisConfig";
+import { showErrorSnackbar } from "utils/snackbarUtils";
+
+interface UploadPhotoResponse {
+  url: string; 
+}
+
+const API_BASE_URL = "/api";
+
+
+const uploadPhoto = async (file: File): Promise<string> => {
+  const formData = new FormData();
+  formData.append('files', file);
+
+  try {
+    const response = await axiosInstance.post<UploadPhotoResponse>(
+      `${API_BASE_URL}/photos`,
+      formData,
+      {
+        headers: {
+          'Content-Type': 'multipart/form-data', 
+        },
+      }
+    );
+
+    return response.data.url;
+  } catch (error) {
+    handleError(error);
+    showErrorSnackbar('Error',"rror uploading image");
+    throw error; 
+  }
+};
+
+export default uploadPhoto;
