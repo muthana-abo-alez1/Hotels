e7327f68ca5232a796e2b0f2a204b72b8046fa3d
muthana-abo-alez1 <92632582+muthana-abo-alez1@users.noreply.github.com>
2024-12-02 14:53:47 +0200

    update url in admin route

--- src/router/AdminRoute.tsx
+++ src/router/AdminRoute.tsx
@@ -1,15 +1,19 @@
 import Admin from "pages/Admin";
+import CitiesPage from "pages/Admin/pages/CitiesPage";
+import HotelsPage from "pages/Admin/pages/HotelsPage";
+import RoomsPage from "pages/Admin/pages/RoomsPage";
 import NotFound from "pages/ErrorPage";
 import { Route, Routes } from "react-router-dom";
 
 const AdminRoute = () => {
   return (
     <Routes>
-      <Route path="/home" element={<Admin />}></Route>
-      <Route path="/search" element={<Admin />}></Route>
-      <Route path="/test" element={<Admin />}></Route>
-      <Route path="/*" element={<NotFound />}></Route>
-
+      <Route path="/" element={<Admin />}>
+      <Route path="cities" element={<CitiesPage />} />
+      <Route path="rooms" element={<RoomsPage />} />
+      <Route path="hotels" element={<HotelsPage />} />
+      </Route>
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 };
