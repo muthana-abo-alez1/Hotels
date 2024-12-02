c47bab57936a3ee1b7a4958fe35b27b1150cf567
muthana-abo-alez1 <92632582+muthana-abo-alez1@users.noreply.github.com>
2024-12-02 15:01:02 +0200

    add implementation rooms apis

---
+++ src/apis/admin/rooms/RoomsApis.ts
@@ -0,0 +1,102 @@
+import { axiosInstance, handleError } from "apis/ApisConfig";
+import { Amenities } from "interfaces/amenities";
+import { Photo } from "interfaces/Photo";
+import { Room, RoomResponse } from "interfaces/Room";
+import { showErrorSnackbar } from "utils/snackbarUtils";
+
+const API_BASE_URL = "/api";
+
+export const getRoomsFromSpecificHotel = async (
+    
+    hotelId: number,
+    checkInDate: string = "test",
+    checkOutDate: string = "test"
+  ): Promise<Room[]> => {
+    try {
+      const response = await axiosInstance.get<Room[]>(
+        `${API_BASE_URL}/hotels/${hotelId}/rooms`,
+        {
+          params: { checkInDate, checkOutDate }, 
+        }
+      );
+      return response.data;
+    } catch (error) {
+      const handledError = handleError(error);
+      showErrorSnackbar("Error", handledError.message);
+      throw handledError;
+    }
+};
+
+export const postRoom = async (
+    hotelId: number,
+    roomNumber: number,
+    cost: number
+  ): Promise<RoomResponse> => {
+    try {
+      const response = await axiosInstance.post<RoomResponse>(
+        `${API_BASE_URL}/hotels/${hotelId}/rooms`,
+        {
+            roomNumber,  
+            cost,     
+          }
+      );
+      return response.data;
+    } catch (error) {
+      const handledError = handleError(error);
+      showErrorSnackbar("Error", handledError.message);
+      throw handledError;
+    }
+};
+
+export const deleteRoom = async (hotelId: number,roomId:number): Promise<Room> => {
+    try {
+      const response = await axiosInstance.delete<Room>( `${API_BASE_URL}/hotels/${hotelId}/rooms/${roomId}`);
+      return response.data;
+    } catch (error) {
+      const handledError = handleError(error);
+      showErrorSnackbar("Error", handledError.message);
+      throw handledError;
+    }
+};
+
+export const postPhotoForRoom = async (
+  roomId: number,
+  url: string,
+): Promise<Photo> => {
+  try {
+    const response = await axiosInstance.post<Photo>(
+      `${API_BASE_URL}/rooms/${roomId}/photos`,
+      {
+        url    
+      }
+    );
+    return response.data;
+  } catch (error) {
+    const handledError = handleError(error);
+    showErrorSnackbar("Error", handledError.message);
+    throw handledError;
+  }
+};
+
+export const postAmenityForRoom = async (
+  roomId: number,
+  name: string,
+  description:string,
+
+): Promise<Amenities> => {
+  try {
+    const response = await axiosInstance.post<Amenities>(
+      `${API_BASE_URL}/rooms/${roomId}/amenities`,
+      {
+        name,
+        description  
+      }
+    );
+    return response.data;
+  } catch (error) {
+    const handledError = handleError(error);
+    showErrorSnackbar("Error", handledError.message);
+    throw handledError;
+  }
+};
+
