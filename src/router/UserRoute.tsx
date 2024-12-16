357361191b08fd22fa4a609b86ec6215fb84eca1
muthana-abo-alez1 <92632582+muthana-abo-alez1@users.noreply.github.com>
2024-12-16 21:01:49 +0200

    update user route (add new pages)

--- src/router/UserRoute.tsx
+++ src/router/UserRoute.tsx
@@ -1,13 +1,22 @@
-import Home from "pages/Home";
+import Home from "pages/User/page/Home";
 import NotFound from "pages/ErrorPage";
 import { Route, Routes } from "react-router-dom";
+import UserPage from "pages/User/page";
+import City from "pages/User/page/City";
+import Hotel from "pages/User/page/Hotel";
+import SearchPage from "pages/User/page/SearchPage";
 
 const UserRoute = () => {
   return (
     <Routes>
-      <Route path="/home" element={<Home />}></Route>
-      <Route path="/*" element={<NotFound />}></Route>
+      <Route path="/" element={<UserPage />}>
+      <Route path="home" element={<Home />}></Route>
+      <Route path="city" element={<City />}></Route>
+      <Route path="hotel/:id" element={<Hotel />} />
+      <Route path="search/data" element={<SearchPage />} />
 
+      </Route>
+      <Route path="*" element={<NotFound />}></Route>
     </Routes>
   );
 };
