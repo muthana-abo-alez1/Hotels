23ebff2d7e2869288a4646a6ab59f37ab89bc05d
muthana-abo-alez1 <92632582+muthana-abo-alez1@users.noreply.github.com>
2024-12-24 15:00:53 +0200

    add date utils

--- src/utils/tokenUtils.ts
+++ src/utils/tokenUtils.ts
@@ -1,8 +1,8 @@
-import { TOKEN_EXPIRY_DAYS } from "constants/Generals";
-import Cookies, { CookieAttributes } from "js-cookie";
+import Cookies from "js-cookie";
 
-export const setToken = (token: string, options: CookieAttributes = { expires: TOKEN_EXPIRY_DAYS }): void => {
-  Cookies.set("token", token, options);
+export const setToken = (token: string): void => {
+  const TOKEN_EXPIRY = new Date(new Date().getTime() + 60 * 60 * 1000);
+  Cookies.set("token", token, { expires: TOKEN_EXPIRY });
 };
 
 export const getToken = (): string | undefined => {
@@ -13,14 +13,14 @@ export const removeToken = (): void => {
   Cookies.remove("token");
 };
 
-export const getUserType = (token : string|null): string|null => {
-  if (!token) return null
+export const getUserType = (token: string | null): string | null => {
+  if (!token) return null;
   try {
-    const parts = token?.split('.');
+    const parts = token?.split(".");
     if (parts?.length !== 3) {
       throw new Error("Invalid token");
     }
-    const payload = atob(parts[1]); 
+    const payload = atob(parts[1]);
     const decodedPayload = JSON.parse(payload);
 
     return decodedPayload.userType;
@@ -29,26 +29,34 @@ export const getUserType = (token : string|null): string|null => {
     return null;
   }
 };
-
-export const getUserId = ():string|null => {
-  const token = getToken()
-  if (!token) return null;
-
+export const decodeToken = (token: string) => {
   try {
-    const parts = token.split('.');
-
-    if (parts.length !== 3) {
-      throw new Error("Invalid token");
-    }
-
-    const base64Url = parts[1];
-    const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/'); 
-    const decodedPayload = JSON.parse(atob(base64));
-
-    return decodedPayload.user_id; 
+    const payload = JSON.parse(atob(token.split(".")[1]));
+    return payload;
   } catch (error) {
     console.error("Failed to decode token:", error);
     return null;
   }
 };
 
+export const getUserId = (): string | null => {
+  const token = getToken();
+  if (!token) return null;
+
+  try {
+    const parts = token.split(".");
+
+    if (parts.length !== 3) {
+      throw new Error("Invalid token");
+    }
+
+    const base64Url = parts[1];
+    const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+    const decodedPayload = JSON.parse(atob(base64));
+
+    return decodedPayload.user_id;
+  } catch (error) {
+    console.error("Failed to decode token:", error);
+    return null;
+  }
+};
