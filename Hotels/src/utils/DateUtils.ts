0918a13ced98e37166a1b4aaee7ec8c660c8b105
muthana-abo-alez1 <92632582+muthana-abo-alez1@users.noreply.github.com>
2024-12-29 15:02:07 +0200

    fix calculateDaysBetweenDates function

--- Hotels/src/utils/DateUtils.ts
+++ Hotels/src/utils/DateUtils.ts
@@ -1,7 +1,10 @@
 export const calculateDaysBetweenDates = (startDate: string, endDate: string): number => {
     const start = new Date(startDate);
     const end = new Date(endDate);
+    start.setHours(0, 0, 0, 0);
+    end.setHours(0, 0, 0, 0);
     const diffInTime = end.getTime() - start.getTime();
-    return Math.max(1, Math.ceil(diffInTime / (1000 * 3600 * 24)));
+    const daysDifference = diffInTime / (1000 * 3600 * 24);
+    if (daysDifference === 0) {return 1;}
+    return Math.floor(daysDifference);
 };
-  
