dd28fd0d1c58ccefffb07bfa6aab4e9fc9e2ec58
muthana-abo-alez1 <92632582+muthana-abo-alez1@users.noreply.github.com>
2025-02-12 16:33:23 +0200

    Fix user blocking issue when opening the Hotel Selection Dialog

--- Hotels/src/pages/Admin/pages/RoomsPage/component/HotelSelectionDialog/HotelSelectionDialog.tsx
+++ Hotels/src/pages/Admin/pages/RoomsPage/component/HotelSelectionDialog/HotelSelectionDialog.tsx
@@ -11,13 +11,15 @@ import {
   InputLabel,
   Typography,
   Box,
+  IconButton,
 } from "@mui/material";
+import ChevronLeft from "@mui/icons-material/ChevronLeft";
 import { useDispatch, useSelector } from "react-redux";
 import { getHotels } from "apis/admin/hotels/HotelsApis";
 import { setHotels } from "../../../../../../redux/reducers/hotelsSlice";
 import { Hotel } from "interfaces/Hotel";
 import { showInfoSnackbar } from "utils/snackbarUtils";
-
+import { useNavigate } from "react-router-dom"; 
 interface HotelSelectionDialogProps {
   open: boolean;
   onClose: (selectedHotel: Hotel | null) => void;
@@ -30,6 +32,7 @@ const HotelSelectionDialog: React.FC<HotelSelectionDialogProps> = ({
   const dispatch = useDispatch();
   const hotels = useSelector((state: any) => state.hotels.hotels);
   const [selectedHotel, setSelectedHotel] = useState<Hotel | null>(null);
+  const navigate = useNavigate(); 
 
   const fetchCities = useCallback(
     async (
@@ -41,26 +44,45 @@ const HotelSelectionDialog: React.FC<HotelSelectionDialogProps> = ({
         const data = await getHotels("", "", pageSize, pageNumber);
         dispatch(setHotels(data));
       } catch (error) {
-        console.error("Error fetching cities:", error);
+        console.error("Error fetching hotels:", error);
       }
     },
     [dispatch]
   );
 
   useEffect(() => {
-      fetchCities();
+    fetchCities();
   }, [fetchCities]);
 
   const handleProceed = () => {
     if (selectedHotel) {
       onClose(selectedHotel);
     } else {
-      showInfoSnackbar("info","Please select a hotel to proceed.")
+      showInfoSnackbar("info", "Please select a hotel to proceed.");
     }
   };
 
   return (
     <Dialog open={open} onClose={() => {}} maxWidth="sm" fullWidth>
+      <IconButton
+        onClick={() => navigate(-1)} 
+        edge="start"
+        sx={{
+          color: "white",
+          position: "fixed",
+          left: { xs: "20px", sm: "20px", md: "48px" },
+          top: "93px",
+          zIndex: 1000,
+          bgcolor: "primary.main",
+          "&:hover": {
+            backgroundColor: "#1976d2",
+          },
+          marginRight: "1rem",
+        }}
+      >
+        <ChevronLeft />
+      </IconButton>
+
       <DialogTitle>Select a Hotel</DialogTitle>
       <DialogContent>
         <Box>
@@ -78,23 +100,28 @@ const HotelSelectionDialog: React.FC<HotelSelectionDialogProps> = ({
                   ) || null
                 )
               }
-              label="Select City"
+              label="Select Hotel"
               color="secondary"
               MenuProps={{
                 PaperProps: {
                   style: {
-                    maxHeight: 300, 
+                    maxHeight: 300,
                     overflowY: "auto",
                   },
                 },
               }}
             >
-
-              {hotels.map((hotel: Hotel) => (
-                <MenuItem key={hotel.id} value={hotel.id}>
-                  {hotel.name}
+              {hotels.length === 0 ? (
+                <MenuItem disabled>
+                  <em>No hotels available</em>
                 </MenuItem>
-              ))}
+              ) : (
+                hotels.map((hotel: Hotel) => (
+                  <MenuItem key={hotel.id} value={hotel.id}>
+                    {hotel.name}
+                  </MenuItem>
+                ))
+              )}
             </Select>
           </FormControl>
         </Box>
@@ -105,7 +132,7 @@ const HotelSelectionDialog: React.FC<HotelSelectionDialogProps> = ({
           color="primary"
           onClick={handleProceed}
           fullWidth
-          sx={{color:"white"}}
+          sx={{ color: "white" }}
         >
           Proceed
         </Button>
